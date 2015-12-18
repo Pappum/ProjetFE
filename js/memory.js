f1ff024df1db3ee344bd376dd064e7abfc03af76
Pappum <Alexis@MacBook-Pro-de-Alexis-2.local>
2015-12-18 10:54:02 +0100

    add image and firsts step

--- js/memory.js
+++ js/memory.js
@@ -1,103 +1,70 @@
-var Morpion = React.createClass({
-getInitialState: function(){
-	return {
-		round: [],
-		cross: [],
-		table: [
-			['','',''],
-			['','',''],
-			['','','']
-		],
-		player: 'round',
-		output: '',
-		win: 'false'
-	};
-},
-play: function(id){
-	return function(){
-		var xy = id.split('-')
-		var x = xy[0]
-		var y = xy[1]
-		var newTable = this.state.table
-		if(newTable[x-1][y-1] != ''){
-			this.setState({output: 'Ce coup n\'est pas permis'})
-			return;                                
-		}
-		if(this.state.player == 'round'){
-			newTable[x-1][y-1] = 'round'
-			var val = this.state.round.concat([id])
-			this.setState({round: val, player: 'cross', table: newTable, output: ''}) 
-			this.checkWin('round', x-1, y-1)
-		}
-		if(this.state.player == 'cross'){
-			newTable[x-1][y-1] = 'cross'
-			var val = this.state.cross.concat([id])
-			this.setState({cross: val, player: 'round', table: newTable, output: ''}) 
-			this.checkWin('cross', x-1, y-1)   
-		} 
-	}.bind(this)
-},
-checkWin: function(player, posX, posY){
-		if(this.state.table[posX][0] == this.state.table[posX][1] && this.state.table[posX][1] == this.state.table[posX][2] && this.state.table[posX][0] == this.state.table[posX][2]){
-			this.setState({output: 'Le joueur '+player+' a gagné', win: 'true'})
-		}
-		if(this.state.table[0][posY] == this.state.table[1][posY] && this.state.table[1][posY] == this.state.table[2][posY] && this.state.table[0][posY] == this.state.table[2][posY]){
-			this.setState({output: 'Le joueur '+player+' a gagné', win: 'true'})
-		}
-		if((this.state.table[0][0] == player && this.state.table[1][1] == player && this.state.table[2][2] == player) && (this.state.table[0][0] == this.state.table[1][1] && this.state.table[0][0] == this.state.table[2][2] && this.state.table[1][1] == this.state.table[2][2] )){
-			this.setState({output: 'Le joueur '+player+' a gagné', win: 'true'})
-		}
-		 if((this.state.table[0][2] == player && this.state.table[1][1] == player && this.state.table[2][0] == player) && (this.state.table[0][2] == this.state.table[1][1] && this.state.table[0][2] == this.state.table[2][0] && this.state.table[1][1] == this.state.table[2][0])){
-			 this.setState({output: 'Le joueur '+player+' a gagné', win: 'true'})
-		 }
-		 var res = 0
-		 for(var i = 0; i < this.state.table.length; i++){
-			for(var j = 0; j < 3; j++){
-				if(this.state.table[i][j] != ''){
-					res = res + 1
-				}
+var Memory = React.createClass({
+	getInitialState: function(){
+		var card = {url: '../img/01.jpg', IsVisible: 1, blocked: 0, playerWinCard: ''};
+		var card2 = {url: '../img/01.jpg', IsVisible: 0, blocked: 0, playerWinCard: ''};
+		return {
+			table: [
+				[card, card2, card],
+				[card, card, card],
+				[card, card, card]
+			],
+			player: '',
+			score: '',
+			backUrl: '../img/back.jpg',
+			win: 'false',
+			FirstClick: '',
+			FirstCard: ''
+		};
+	},
+	play: function(x, y) {
+		return function() {
+			var newTable = this.state.table
+			console.log(newTable[x][y])
+			if(newTable[x][y].IsVisible == 0) {
+				newTable[x][y].IsVisible = 1
+				this.setState({table: newTable})
+				console.log(newTable[x][y])
 			}
-		 }
-		if(res == 9){
-			this.setState({output: 'Match nul', win: 'true'})
-		}
-		console.log(res)
-},
-Restart: function(){
-	this.setState(this.getInitialState())
-},
-render: function(){
-	return(
-		 <div className="container">
-			<div className="output">
-					<p>{this.state.output}</p>
-					{
-						this.state.win == 'true' ? <button onClick={this.Restart}>Restart</button> : null
-					}
-			</div>
-			{[1, 2, 3].map(function(x){
-				return [1, 2, 3].map(function(y){
-				var id = x+'-'+y
+			else {
+				newTable[x][y].IsVisible = 0
+				this.setState({table: newTable})
+				console.log(newTable[x][y])
+			}
+		}.bind(this)
+	},
+	render: function(){
+		return(
+			 <div className="container">
+				{this.state.table.map(function(_, x){
+					return this.state.table[x].map(function(_, y){
+					var id = x+'-'+y
 
-				var style = 'cell'
-			
-					if(this.state.table[x-1][y-1] == 'round'){
-						style += " round"
+					var src
+					if (this.state.table[x][y].IsVisible == 1) {
+						src = this.state.table[x][y].url
 					}
-					if(this.state.table[x-1][y-1] == 'cross'){
-						style += " cross"
+					else {
+						src = this.state.backUrl
 					}
-				return (
-					<div className={style} id={id} onClick={this.play(id)}>
-					</div>
-				) 
-			}.bind(this))}.bind(this))}
-		 </div>
-	);
-}
+
+					return (
+						<div className='cell' id={id} onClick={this.play(x, y)}>
+							<img src={src} />
+						</div>
+					) 
+				}.bind(this))}.bind(this))}
+			 </div>
+		);
+	}
 });
 
+var ar = Array.apply(null, Array(8)).map(Number.prototype.valueOf,0);
+var urls = ar.map(function(_, i) {
+	return '../img/0'+i+'.jpg'
+})
+console.log(urls)
+
 ReactDOM.render(
-	<Morpion />,
-	document.getElementById('morpion')
+	<Memory urls={urls}/>,
+	document.getElementById('memory')
 );
