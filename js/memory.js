8fae3637fa2cb2b28efae32f3acd9fe3080cb78f
Pappum <Alexis@MacBook-Pro-de-Alexis-2.local>
2016-01-11 11:56:19 +0100

    youyou ça marche plutôt pas mal

--- js/memory.js
+++ js/memory.js
@@ -2,11 +2,11 @@ var Memory = React.createClass({
 	getInitialState: function(){
 		return {
 			table: this.generateCards(),
-			player: '',
+			player: 1,
 			score: '',
 			backUrl: 'img/back.jpg',
 			win: 'false',
-			firstClick: true,
+			clickCount: 0,
 			firstCard: ''
 		};
 	},
@@ -50,41 +50,102 @@ var Memory = React.createClass({
 
 			var firstCardProps
 
-			if (newTable[x][y].blocked == true) {
-				alert('nein');
+			if (newTable[x][y].blocked == true || newTable[x][y] == this.state.firstCard) {
+				alert('impossible');
 			}
 			else {
-				if(this.state.firstClick == true) {
+				if(this.state.clickCount == 0) {
 					firstCardProps = newTable[x][y]
 
 					newTable[x][y].isVisible = 1
-					this.setState({firstClick: false, firstCard: firstCardProps, table: newTable})
+					this.setState({clickCount: 1, firstCard: firstCardProps, table: newTable})
+				}
+				else if (this.state.clickCount == 2) {
+					alert('attends');
 				}
 				else {
 					newTable[x][y].isVisible = 1
-					this.setState({firstClick: true, table: newTable})
+					this.setState({clickCount: 2, table: newTable})
 
 					if(newTable[x][y].url == this.state.firstCard.url) {
-						newTable[x][y].blocked = true;
 						var PC = this.state.firstCard;
+
+						newTable[x][y].blocked = true;
 						PC.blocked = true;
+
+						newTable[x][y].playerWinCard = this.state.player;
+						PC.playerWinCard = this.state.player;
+
 						alert('champion');
+						this.setState({table: newTable, clickCount: 0})
 					}
 					else {
-						newTable[x][y].isVisible = 0;
 						var PC = this.state.firstCard;
-						PC.isVisible = 0;
+						var self = this
+						var player
+						setTimeout(function() {
+							newTable[x][y].isVisible = 0;
+							PC.isVisible = 0;
+							self.state.player == 1 ? player = 2 : player = 1;
+							self.setState({table: newTable, clickCount: 0, player: player})
+						}, 1000);
 					}
 				}
 			}
 
-			console.log(newTable[x][y]);
+			console.log(newTable[x][y])
 
 		}.bind(this)
+	},
+	getScore: function(player) {
+		var tableInOne = _.flatten(this.state.table, true);
+		var tableScore = tableInOne.filter(
+			function(obj) {
+				if (obj.playerWinCard == player) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		);
+		var score = tableScore.length / 2
+		return score;
+
+	},
+	getAllFlipped: function() {
+		var tableInOne = _.flatten(this.state.table, true);
+		var tableFlipped = tableInOne.filter(
+			function(obj) {
+				if (obj.blocked == 1) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		);
+		console.log(tableFlipped.length)
+		if(tableFlipped.length == 16) {
+			var winner
+			if(this.getScore(1) > this.getScore(2)) {
+				winner = 'Joueur 1'
+			}
+			else if(this.getScore(1) = this.getScore(2)) {
+				winner = 'Egalité'
+			}
+			else {
+				winner = 'Joueur 2'
+			}
+			return winner;
+		}
 	},
 	render: function(){
 		return(
 			 <div className="container">
+			 	<div className="score">
+			 		<p>Score joueur 1 = {this.getScore(1)}</p>
+			 		<p>Score joueur 2 = {this.getScore(2)}</p>
+			 		<p>All flipped : {this.getAllFlipped()}</p>
+			 	</div>
 				{this.state.table.map(function(_, x){
 					return this.state.table[x].map(function(_, y){
 					var id = x+'-'+y
